e14ef8dac82b852ea9a0f4ca7fd691b3e7c7f382
Hellen Ward <hellenwellsward@gmail.com>
2019-04-13 08:11:50 +0100

    Put in the option to have a reversed card.  Fixed some of the images as they were in the incorrect order.  Started a stylesheet to rotate card if it's reversed.

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 $(document).ready(function() {
 
-  const oneCardSpace = document.querySelector(".readingResult");
+  const readingResultOneCard = (card) => {
+    const isReversed = Math.floor(Math.random() * 2);
+
+    if (isReversed === 0) {
+      $(".readingResult").append(`<div>
+        <img src="${card.image}">
+        <h3>${card.name}</h3>
+        <p>${card.definition}</p>
+        </div>`)
+    } else {
+      $(".readingResult").append(`<div>
+        <img src="${card.image}" class="reversed">
+        <h3>${card.name} - Reversed</h3>
+        <p>${card.reversed}</p>
+        </div>`)
+    }
+  }
 
   $('.oneCardButton').click(function(){
+      $('.clearMe').empty();
       const cardOptions = window.cards;
       let chosenCard = Math.floor(Math.random() * cardOptions.length);
-      console.log(cardOptions[chosenCard]);
+      readingResultOneCard(cardOptions[chosenCard]);
     })
 })
