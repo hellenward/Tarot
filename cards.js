8685a64e7924f87df45b76fe40eab26cae3b39c9
Hellen Ward <hellenwellsward@gmail.com>
2019-04-15 07:17:57 +0100

    Entered in some more card meanings

--- cards.js
+++ cards.js
@@ -2,36 +2,266 @@ window.cards = [
   {
     "number": 1,
     "name": "The Magician",
-    "definition" : "The start of something. Beginner’s luck. Having various tools and means at our disposal. Use of supernatural forces. Creating reality with mind power. Training and acquisition of practical skills. Improvisation. Display or show for other people. Message: create a new reality.",
+    "definition" : "The start of something. Beginner’s luck. Having various tools and means at our disposal. Use of supernatural forces. Creating reality with mind power. Training and acquisition of practical skills. Improvisation. Display or show for other people.",
     "reversed": "Trickery, sleight of hand, cheating. Showing off, pretending. Lack of self-awareness about body, sexuality, or basic motives. Near miss due to inexperience or inaccuracy.",
+    "message": "Message: create a new reality.",
     "image": "./Cards/a01.jpg"
   },
   {
     "number": 2,
-    "name": "The Empress",
-    "definition": "Abundance, growth, productivity. Natural or human touch within an artificial framework. Emotional intelligence. Protection and care. Motherhood. A powerful female figure. Strong feminine identity. Message: act from the guts.",
-    "reversed": "Impulsive behavior, someone difficult to reason with. Over-protectiveness, excessive involvement in the life of others. Problems with a strong mother figure.",
+    "name": "The High Priestess",
+    "definition": "Wisdom combining intellect and intuition. A spiritual mother. A woman hiding her strengths in a world of men. Modesty. Secrets, something hidden, mystery. Getting a hint of something which remains largely unknown. Impossible to give a definite answer now.",
+    "reversed": "Need to hide our true nature behind the conventions of normal society. Conservative approach to sex and the body. Emotional blockage.",
+    "message": "Message: know how to set boundaries.",
     "image": "./Cards/a02.jpg"
   },
   {
     "number": 3,
-    "name": "The Emperor",
-    "definition": "Practical and material achievements. Matters relating to the workplace or source of income. Authority and control, a commanding position. A protective father figure, patron or sponsor. Assertiveness. Military affairs. Message: show leadership and responsibility",
-    "reversed": "Belligerence, violence, trying to solve things by brute force. Dictatorship. Possibility of sexual abuse. Difficulty in coping with a dominant father figure. Denial and hiding of inner weaknesses.",
-    "image": "./Cards/a04.jpg"
+    "name": "The Empress",
+    "definition": "Abundance, growth, productivity. Natural or human touch within an artificial framework. Emotional intelligence. Protection and care. Motherhood. A powerful female figure. Strong feminine identity.",
+    "reversed": "Impulsive behavior, someone difficult to reason with. Over-protectiveness, excessive involvement in the life of others. Problems with a strong mother figure.",
+    "message": "Message: act from the guts.",
+    "image": "./Cards/a03.jpg"
   },
   {
     "number": 4,
-    "name": "The High Priest",
-    "definition": "Teacher, instructor, or counselor. Education and knowledge, academic expertise. Organized religion, conventional medicine or psychology. Spiritual father. Consultation or treatment by a specialist. Marriage. Message: respect knowledge and education.",
-    "reversed": "Excessive adhesion to conventions and outdated norms. Bureaucracy, an oppressive establishment. Hypocrisy, discrimination. Divorce.",
-    "image": "./Cards/a05.jpg"
+    "name": "The Emperor",
+    "definition": "Practical and material achievements. Matters relating to the workplace or source of income. Authority and control, a commanding position. A protective father figure, patron or sponsor. Assertiveness. Military affairs.",
+    "reversed": "Belligerence, violence, trying to solve things by brute force. Dictatorship. Possibility of sexual abuse. Difficulty in coping with a dominant father figure. Denial and hiding of inner weaknesses.",
+    "message": "Message: show leadership and responsibility.",
+    "image": "./Cards/a04.jpg"
   },
   {
     "number": 5,
+    "name": "The High Priest",
+    "definition": "Teacher, instructor, or counselor. Education and knowledge, academic expertise. Organized religion, conventional medicine or psychology. Spiritual father. Consultation or treatment by a specialist. Marriage.",
+    "reversed": "Excessive adhesion to conventions and outdated norms. Bureaucracy, an oppressive establishment. Hypocrisy, discrimination. Divorce.",
+    "message": "Message: respect knowledge and education.",
+    "image": "./Cards/a05.jpg"
+  },
+  {
+    "number": 6,
     "name": "The Lovers",
-    "definition": "Love, amorous relationship. Emotional entanglement. Need to make a choice, or to disengage oneself from past influences. Inclinations of the heart correspond to the will of heaven. Small steps actually taken are the visible signs of inner desire. Message: follow the path of the heart.",
+    "definition": "Love, amorous relationship. Emotional entanglement. Need to make a choice, or to disengage oneself from past influences. Inclinations of the heart correspond to the will of heaven. Small steps actually taken are the visible signs of inner desire.",
     "reversed": "Complex relationship between several people, e.g., a romantic triangle or a tension between mother and wife. Hesitation, quandary. Confusion as to one’s own feeling and will.",
+    "message": "Message: follow the path of the heart.",
     "image": "./Cards/a06.jpg"
+  },
+  {
+    "number": 7,
+    "name": "The Chariot",
+    "definition": "Victory or an achievement putting the querent in a strong and protected position. Ambition, energy, motivation to move forward. Public honor. Power and high status.",
+    "reversed": "Inner weakness hidden behind external show-off. Arrogance, vanity. Over-protectiveness, emotional closure. Confusion about one’s goals. Losing the simple touch with people and reality.",
+    "message": "Message: dare and win.",
+    "image": "./Cards/a07.jpg"
+  },
+  {
+    "number": 8,
+    "name": "Justice",
+    "definition": "Law and order, legal and court issues. A fair and balanced judgment. A developed conscience. Rationality, reasoning by clear rules and common norms. Touch of grace and humanity beyond the objective considerations.",
+    "reversed": "Petty accountability, a critical and judgmental attitude, guilt feelings. Repressive control of self and of others. Negative ideas blocking change and advance.",
+    "message": "Message: act with reason and by the accepted norms.",
+    "image": "./Cards/a08.jpg"
+  },
+  {
+    "number": 9,
+    "name": "The Hermit",
+    "definition": "A quest for truth or for spiritual understanding. Concentrating on a clear purpose. Caution, careful examination. Self-privation for the sake of a meaningful cause. Loyalty to principles, strong faith.",
+    "reversed": "A closed and reclusive attitude. Isolation, loneliness. Fixed ideas. Excessive caution and suspicion, a critical approach looking for defects. Hidden and denied desires.",
+    "message": "Message: look for the essence of things.",
+    "image": "./Cards/a09.jpg"
+  },
+  {
+    "number": 10,
+    "name": "The Wheel of Fortune",
+    "definition": "Change in circumstances and position. A rise after a fall. Gambling, putting faith in capricious luck. Life cycles, closure of circles. Adapting to the routine of everyday life. A hint to previous incarnations.",
+    "reversed": "A decline after a period of rising. Danger lurks at the summit. Moving in a closed circle. Capricious mood changes. Feeling powerless to affect one’s situation.",
+    "message": "Message: accept life’s ups and downs.",
+    "image": "./Cards/a10.jpg"
+  },
+  {
+    "number": 11,
+    "name": "Strength",
+    "definition": "Power and courage to face challenges. Controlled expression of creative urges, drives and desires. Mobilization of inner resources towards a common goal. Taking risks.",
+    "reversed": "Need to keep things under control leads to constant tensions. A risk of losing one’s grip. Internal conflicts and unrealistic assessment of one’s own forces may lead to failure.",
+    "message": "Message: take control of yourself.",
+    "image": "./Cards/a11.jpg"
+  },
+  {
+    "number": 12,
+    "name": "The Hanged Man",
+    "definition": "Seeing things from a unique point of view. Enduring difficulties for a worthy cause. A period of deep self examination. Passivity, acceptance of reality even if it is the opposite of what one expects.",
+    "reversed": "Isolation. Emotional stance of a victim. Inability to act. Denying one’s own unique qualities, striving to be “normal” at all costs. Living in one’s private and imaginary reality.",
+    "message": "Message: look at things from the opposite perspective.",
+    "image": "./Cards/a12.jpg"
+  },
+  {
+    "number": 13,
+    "name": "Death",
+    "definition": "The end of something whose time has come. Cutting off past influences or attachment to dominant figures. Giving up the superfluous and keeping only the essential. Disintegration of the old makes room for the new.",
+    "reversed": "Difficulty in coping with loss or change. Temporary difficulties, a trying challenge. Disintegration. Realization of a painful truth. Does not predict future death, but may reflect anxiety about dying or mourning over a loss which has already happened.",
+    "message": "Message: give up what is over.",
+    "image": "./Cards/a13.jpg"
+  },
+  {
+    "number": 14,
+    "name": "Temperance",
+    "definition": "Reconciliation, compromise, relaxation of tensions. Integration of opposites. Ability to do the seemingly impossible. A slow process of distillation and improvement. Patience, perseverance. Self-improvement.",
+    "reversed": "Going back and forth without making real progress. Losing patience with a lengthy process. Emotional preoccupation with oneself, pushing away others who might come to help.",
+    "message": "Message: find the golden mean.",
+    "image": "./Cards/a14.jpg"
+  },
+  {
+    "number": 15,
+    "name": "The Devil",
+    "definition": "A burst of creativity. Paradoxes and contradictions. Irony and mocking of common norms. Acting from desires, passions and impulses. Moving on from a past family trauma.",
+      "reversed": "Temptation, attraction to the dark and forbidden. Exploitation, egotism, domination. Compulsive self-gratification. Senseless behavior has its price. Difficulty in detaching oneself from an unhealthy bond.",
+    "message": "Message: express passion and desire.",
+    "image": "./Cards/a15.jpg"
+  },
+  {
+    "number": 16,
+    "name": "The Tower",
+    "definition": "Breaking up of solid structures. Getting free from confinement. Sudden breakthrough after long preparations. Sparkling sexual encounter. Success lies in simplicity and modesty.",
+    "reversed": "Shock, collapse of projects or trusted structures. A fall from an apparently solid and secure position. Chaos, confusion, difficulty in understanding what is going on. Vanity and pride lead to failure.",
+    "message": "Message: return to the solid ground of reality.",
+    "image": "./Cards/a16.jpg"
+  },
+  {
+    "number": 17,
+    "name": "The Star",
+    "definition": "Openness, simplicity, return to nature. Purity, honesty. Showing yourself “as you are,” accepting one’s body and desires. Generosity. Luck from heaven. Intuitive feeling of guidance or energy coming from a higher plane.",
+    "reversed": "Naive optimism and wishful-thinking. Exposing oneself to danger or abuse. Difficulty in setting proper boundaries. Squandering, wastefulness.",
+    "message": "Message: flow from a pure source.",
+    "image": "./Cards/a17.jpg"
+  },
+  {
+    "number": 18,
+    "name": "The Moon",
+    "definition": "Deep emotions, perhaps related to a mother or feminine figure. A different experience of reality. Longing for the unreachable. Finding one’s hidden strengths. Occupation with the remote past. A hidden treasure.",
+    "reversed": "Vague and disturbing feelings. Emotional difficulties, a period of depression. Danger lurking under the surface. Retreat, the road ahead is hard to find.",
+    "message": "Message: don’t be afraid to go deep down.",
+    "image": "./Cards/a18.jpg"
+  },
+  {
+    "number": 19,
+    "name": "The Sun",
+    "definition": "Light and warmth, abundance, blessings. Pleasant feeling, emotional or physical healing. Partnership, trust, sharing, brotherhood. Human touch. An ideal father figure. Matters relating to children. Setting limits in a moderate and non-oppressive way.",
+    "reversed": "Living in a limited space, difficulty to face reality “in the open.” Immaturity, dependence on others. Someone or something too intense and energetic to feel comfortable with. An absent father.",
+    "message": "Message: find suitable partners.",
+    "image": "./Cards/a19.jpg"
+  },
+  {
+    "number": 20,
+    "name": "Judgement",
+    "definition": "Revelation, enlightenment, a new understanding. A turning point in a therapy process. Healing of a family relationship. Disclosure, secrets revealed, publicity. Birth of a baby or of a new thing.",
+    "reversed": "Revelation of something that should have been kept hidden. Lack of privacy. Unpleasant realization. Problems related to child-parent relations. Too much noise and drama.",
+    "message": "Message: awaken to spiritual reality.",
+    "image": "./Cards/a20.jpg"
+  },
+  {
+    "number": 21,
+    "name": "The World",
+    "definition": "Completion of a process. Balanced activity and achievements in various domains. Contact with far places. Harmony and correspondence between different planes. Pregnancy, something new is about to be born. The Dance of Life.",
+    "reversed": "Life in a bubble, difficulty in sharing your world with others. Disconnection of inner feelings from external life. Preoccupation with oneself, idealized self-image, inability to move forward.",
+    "message": "Message: everything is perfect as it is.",
+    "image": "./Cards/a21.jpg"
+  },
+  {
+    "number": 21,
+    "name": "The Fool",
+    "definition": "Freedom from conventions and norms. Something or someone unique and exceptional. Options kept open. Giving up control, spontaneity. Uncertainty, attention to the here and now. Going on a trip.",
+    "reversed": "Difficulty in choosing and committing oneself to something stable. Restlessness. Lack of purpose. Getting lost. Foolish behavior. Eccentricity, lack of acceptance by the social environment. Difficulty in planning ahead.",
+    "message": "Message: keep on the move.",
+    "image": "./Cards/a22.jpg"
+  },
+  {
+    "number": 22,
+    "name": "Ace of Coins",
+    "definition": "A good start in material things. Financial and physical stability. A practical perspective. A significant sum of money. Utilitarian approach. Greed. Something basic and unsophisticated.",
+    "reversed": "Worrying about money, perhaps unnecessarily.  Count your blessings.",
+    "message": "Message: A new venture in work or finance.",
+    "image": "./Cards/d01.jpg"
+  },
+  {
+    "number": 23,
+    "name": "Two of Coins",
+    "definition": "Duality. Two options or two elements. Collaborating while keeping distance. A winding road, advancing in complex ways. Recognition and acknowledgment.",
+    "reversed": "You may be overstretching yourself or overinvesting in one area of life. Consider more structure and organisation to better meet your resonsibilities.",
+    "message": "Message: be aware of the concept of balance",
+    "image": "./Cards/d02.jpg"
+  },
+  {
+    "number": 24,
+    "name": "Three of Coins",
+    "definition": "Product. A partnership or an alliance bears fruit. First results of a project. Good prospects.",
+    "reversed": "Disappointment, a partnership or a project does not bear the expected fruits.",
+    "message": "Message: Reaping the results of your labour",
+    "image": "./Cards/d03.jpg"
+  },
+  {
+    "number": 25,
+    "name": "Four of Coins",
+    "definition": "Stability. Solid material assets. Tradition, reputation and honor. Time-tested reliability. Established social institutions.",
+    "reversed": "Conservatism, clinging to old and outdated patterns.",
+    "message": "Message: Structure, stability.",
+    "image": "./Cards/d04.jpg"
+  },
+  {
+    "number": 26,
+    "name": "Five of Coins",
+    "definition": "Disruption. Something new appears and destabilizes existing structures. A new element gets attention, but also awakens resistance.",
+    "reversed": "Recovery from financial hardship, overcoming adversity, recovery in health.",
+    "message": "Message: financial challenges",
+    "image": "./Cards/d05.jpg"
+  },
+  {
+    "number": 27,
+    "name": "Six of Coins",
+    "definition": "Expansion. Abundance of resources and possible ways to advance. A positive outlook, success. A good balance between stability and movement.",
+    "reversed": "Don't be too proud or stubborn to allow others to help you.",
+    "message": "Message: generosity",
+    "image": "./Cards/d06.jpg"
+  },
+  {
+    "number": 28,
+    "name": "Seven of Coins",
+    "definition": "Acceptance. Something new is well received. Help and protection. Integrating into a system without losing one’s individuality.",
+    "reversed": "Lack of independence, need to rely on help and acceptance from others.",
+    "message": "Message: a good financial omen",
+    "image": "./Cards/d07.jpg"
+  },
+  {
+    "number": 29,
+    "name": "Eight of Coins",
+    "definition": "Uniformity. A mechanical structure. Practical considerations prove efficient, but lack a human touch. Routine work. A slow and patient advance.",
+    "reversed": "Repetitive or boring work, shoddy workmanship, dead end career",
+    "message": "Message: rewards from your efforts.",
+    "image": "./Cards/d08.jpg"
+  },
+  {
+    "number": 30,
+    "name": "Nine of Coins",
+    "definition": "Motivation. Carving a niche for oneself in an existing system. Ambition. Endurance and independent thinking bear long-term fruits.",
+    "reversed": "Lack of independence, gold-digger, failure, lack of self-discipline.",
+    "message": "Message: financial success through hard work.",
+    "image": "./Cards/d09.jpg"
+  },
+  {
+    "number": 31,
+    "name": "Ten of Coins",
+    "definition": "Abundance. Intensive activity in practical affairs. Material success and achievements. Some may be getting more than others.",
+    "reversed": "Shaky foundations, a hint of dishonesty, financial disaster.",
+    "message": "Message: financial abundance.",
+    "image": "./Cards/d10.jpg"
+  },
+  {
+    "number": 32,
+    "name": "Page of Coins",
+    "definition": "A practical endeavor. Untapped potentials are within reach. Tangible success at the beginning. A solid material base for further advancement.",
+    "reversed": "hesitation, lack of clear purpose. Thinking in terms of past achievements misses present opportunities..",
+    "message": "Message: tap into your ability to manifest a personal goal or dream.",
+    "image": "./Cards/d11.jpg"
   }
+
 ]
