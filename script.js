94adced20528e6fee923c10c27fbb1b90ccc481f
Hellen Ward <hellenwellsward@gmail.com>
2019-04-16 20:50:11 +0100

    Added in a horseshoe spread option

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
 
+const cardOptions = window.cards;
+const newReadingArray = cardOptions.slice();
+const cardsToShow = [];
+
 const isReversed = () =>{
   return Math.floor(Math.random() * 2) === 1;
 }
@@ -45,13 +49,34 @@ const revealHidden = () => {
           <p>${card.reversed}</p>
           </div>`)
       }
+    })
+  }
 
+  const readingResultHorseshoe = (cardsToShow) => {
+    revealHidden();
+    const wordsHorseshoe = ["Past", "Present", "Hidden Influence", "About You", "The Influence of Others", "Suggested Action", "The Final Outcome"];
+    cardsToShow.forEach(function(card, index) {
+      const title = wordsHorseshoe[index];
+      if(!isReversed()) {
+        $(".readingResult").append(`<div>
+          <h2>${title}</h2>
+          <img src="${card.image}">
+          <h3>${card.name}</h3>
+          <p>${card.definition}</p>
+          </div>`)
+      } else {
+        $(".readingResult").append(`<div>
+          <h2>${title}</h2>
+          <img src="${card.image}" class="reversed">
+          <h3>${card.name} - Reversed</h3>
+          <p>${card.reversed}</p>
+          </div>`)
+        }
     })
   }
 
   $('.readingButton').click(function(){
       $('.clearMe').empty();
-      const cardOptions = window.cards;
       readingSelection = $('.readingSelection').val();
       if (readingSelection === "select") {
         alert("You must choose a reading!");
@@ -59,15 +84,20 @@ const revealHidden = () => {
       } else if (readingSelection === "oneCard") {
         let chosenCard = Math.floor(Math.random() * cardOptions.length);
         readingResultOneCard(cardOptions[chosenCard]);
-      } else {
-        const newReadingArray = cardOptions.slice();
-        const cardsToShow = [];
+      } else if (readingSelection === "threeCard") {
         for (let i=0; i < 3; i++) {
           let chosenCard = Math.floor(Math.random() * newReadingArray.length);
           cardsToShow.push(newReadingArray[chosenCard]);
           newReadingArray.splice(chosenCard, 1);
         }
         readingResultThreeCards(cardsToShow);
-      }
+      } else if (readingSelection === "horseshoe") {
+        for (let i=0; i < 7; i++) {
+          let chosenCard = Math.floor(Math.random() * newReadingArray.length);
+          cardsToShow.push(newReadingArray[chosenCard]);
+          newReadingArray.splice(chosenCard, 1);
+        }
+        readingResultHorseshoe(cardsToShow);
+      } 
     })
 })
